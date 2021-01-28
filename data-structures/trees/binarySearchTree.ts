2b74f6e3fb6fb77aa411326c7c835e0cb34583d5
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-01-28 17:00:53 +0100

    add the remove function to the binary search tree

--- data-structures/trees/binarySearchTree.ts
+++ data-structures/trees/binarySearchTree.ts
@@ -75,20 +75,95 @@ class BinarySearchTree {
 
 		return null
 	}
+
+	remove(value: number) {
+		if (!this.root) {
+			return false
+		}
+
+		let currentNode = this.root
+		let parentNode: Node = null
+
+		while (currentNode) {
+			if (currentNode.value > value) {
+				parentNode = currentNode
+				currentNode = currentNode.left
+			} else if (currentNode.value < value) {
+				parentNode = currentNode
+				currentNode = currentNode.right
+			} else if (currentNode.value === value) {
+				// We have a match
+
+				// Option 1: No right Child
+				if (currentNode.right === null) {
+					if (parentNode === null) {
+						this.root = currentNode.left
+					} else {
+						// if parent > current value, make current left child a child of parent
+						if (currentNode.value < parentNode.value) {
+							parentNode.left = currentNode.left
+						}
+
+						// if parent < current value, make current left child a right child of parent
+						if (currentNode.value > parentNode.value) {
+							parentNode.right = currentNode.left
+						}
+					}
+				}
+
+				// Option 2: Right child which doesn't have a left child
+				else if (currentNode.left === null) {
+					if (parentNode === null) {
+						this.root = currentNode.left
+					} else {
+						currentNode.right.left = currentNode.left
+
+						// if parent > current value, make right child of the left a parent
+						if (currentNode.value < parentNode.value) {
+							parentNode.left = currentNode.right
+						}
+
+						// if parent < current value, make right child a right child of the parent
+						else if (currentNode.value > parentNode.value) {
+							parentNode.right = currentNode.right
+						}
+					}
+				}
+
+				// Option 3: Right child that has a left child
+				else if (currentNode.left === null) {
+					// find the right child's left most child
+					let leftMost = currentNode.right.left
+					let leftMostParent = currentNode.right
+
+					while (leftMost.left !== null) {
+						leftMostParent = leftMost
+						leftMost = leftMost.left
+					}
+
+					// Parent's left subtree is now leftmost's right subtree
+					leftMostParent.left = leftMost.right
+					leftMost.left = currentNode.left
+					leftMost.right = currentNode.right
+
+					if (parentNode === null) {
+						this.root = leftMost
+					} else {
+						if (currentNode.value < parentNode.value) {
+							parentNode.left = leftMost
+						} else if (currentNode.value > parentNode.value) {
+							parentNode.right = leftMost
+						}
+					}
+				}
+
+				return true
+			}
+		}
+	}
 }
 
 const tree = new BinarySearchTree()
-tree.insert(9)
-tree.insert(4)
-tree.insert(6)
-tree.insert(20)
-tree.insert(170)
-tree.insert(15)
-tree.insert(1)
-console.log(tree.lookup(445))
-console.log(tree.lookup(20))
-
-// console.log(JSON.stringify(traverse(tree.root)))
 
 function traverse(node: Node) {
 	const tree: Node = { value: node.value }
